cd2016a91ec52700bc25491c2ee1d8288c7236ef
Elijah Gardner Woods <gardnerwoods@wisc.edu>
2025-09-13 15:30:27 -0500

    Adjusted index.html, changed main.js to align with homework examples, and debugged main_with_debug.js.
    In index.html: changed header color to "bisque", changed title to "Greetings!", streamlined the code that adds "Hello World!" at the end of the title, and changed  which JavaScript file the HTML reads (now it's main_with_debug.js).
    
    In main.js: changed most of the code to align with the homework reading examples.
    
    In main_with_debug.js: debugged the code so it works.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,73 @@
-/* JavaScript by Elijah Gardner Woods, 2025 */
+//initialize function called when the script loads
+function initialize(){
+    cities();
+};
+
+//function to create a table with cities and their populations
+function cities(){
+    //define two arrays for cities and population
+    var cityPop = [
+    { 
+        city: 'Madison',
+        population: 233209
+    },
+    {
+        city: 'Milwaukee',
+        population: 594833
+    },
+    {
+        city: 'Green Bay',
+        population: 104057
+    },
+    {
+        city: 'Superior',
+        population: 27244
+    }]
+
+    //create the table element
+    var table = document.createElement("table");
+
+    //create a header row
+    var headerRow = document.createElement("tr");
+
+    //add the "City" and "Population" columns to the header row
+    headerRow.insertAdjacentHTML("beforeend","<th>City</th><th>Population</th>")
+
+    //add the row to the table
+    table.appendChild(headerRow);
+
+    //loop to add a new row for each city
+    for(var i = 0; i < cityPop.length; i++){
+        //assign longer html strings to a variable
+        var rowHtml = "<tr><td>" + cityPop[i].city + "</td><td>" + cityPop[i].population + "</td></tr>";
+        //add the row's html string to the table
+        table.insertAdjacentHTML('beforeend',rowHtml);
+    }
+
+    document.querySelector("#mydiv").appendChild(table);
+}
+
+//change the text color
+    document.querySelector('#mydiv').style.color = 'red';
+
+    //change the text size and alignment
+    document.querySelector('#mydiv').style.fontSize = '2em';
+    document.querySelector('#mydiv').style.textAlign = 'left';
+
+    //get the text color and add it as text to the div
+    var thecolor = document.querySelector('#mydiv').style.color;
+    document.querySelector('#mydiv').insertAdjacentHTML('beforeend',thecolor);
+document.addEventListener('DOMContentLoaded',initialize)
+
+//click listener with anonymous handler function
+document.querySelector("table").addEventListener("click", function(){
+    alert('Madison Rocks! Go Badgers!');
+});
+
+//named handler function for removable listener
+function clickme(){
+    alert('Yeah Green Bay! Go Packers!');
+};
+
+//add the event listener
+document.querySelector("table").addEventListener('click', clickme);
---
+++ js/main_with_debug.js
@@ -0,0 +1,90 @@
+// run the initialize function.
+initialize();
+
+//creates the cityPop variable and loads in the city data.
+function initialize(){
+    var cityPop = [
+		//items inside the variable.
+        {city: 'Madison', population: 233209},
+        {city: 'Milwaukee', population: 594833},
+        {city: 'Green Bay', population: 104057},
+        {city: 'Superior', population: 27244}
+    ];
+
+    //calls the functions created below.
+    cities(cityPop);
+    addColumns(cityPop);
+    addEvents();
+}
+
+//creates the table with cities and their populations
+function cities(cityPop){
+	//creates the table.
+    var table = document.createElement("table");
+	//creates the top row for the table.
+    var headerRow = document.createElement("tr");
+    
+	//adds titles for the rows City and Population and appends them to the table header.
+    headerRow.insertAdjacentHTML("beforeend","<th>City</th><th>Population</th>");
+    table.appendChild(headerRow);
+    
+	//add rows and city data to the table.
+    for (var i = 0; i < cityPop.length; i++){
+        var rowHtml = "<tr><td>" + cityPop[i].city + "</td><td>" + cityPop[i].population + "</td></tr>";
+        table.insertAdjacentHTML('beforeend', rowHtml);
+    }
+    
+    //links the table to the HTML.
+    document.querySelector("#mydiv").appendChild(table);
+}
+
+/*seperately creates the City Size column and fills it with city size data, catigorizing
+based on city population size.*/
+function addColumns(cityPop){
+    document.querySelectorAll("tr").forEach(function(row, i){
+        if (i === 0){
+            row.insertAdjacentHTML('beforeend', '<th>City Size</th>');
+        } else {
+            var citySize;
+			//applies 'Small' label to smaller cities.
+            if (cityPop[i-1].population < 100000){
+                citySize = 'Small';
+			//applies 'Medim' label to medium-sized cities cities.
+            } else if (cityPop[i-1].population < 500000){
+                citySize = 'Medium';
+			//applies 'Large' label to large cities.
+            } else {
+                citySize = 'Large';
+            }
+			//inserts label into row.
+            row.insertAdjacentHTML('beforeend', '<td>' + citySize + '</td>');
+        }
+    });
+}
+
+/*creates two event listeners, one for changing the table's text color when moused over 
+ and one for displaying a message when the table is clicked on.*/
+function addEvents(){
+	//specifies the first event listener to work when the table is moused over.
+    document.querySelector("table").addEventListener("mouseover", function(){
+		//selects random colors for the table to change to.
+        var color = "rgb(";
+        for (var i=0; i<3; i++){
+            var random = Math.round(Math.random() * 255);
+            color += random;
+            if (i<2){
+                color += ",";
+            } else {
+                color += ")";
+            }
+        }
+		//applies the event listener to the table.
+        document.querySelector("table").style.color = color;
+    });
+    
+	/*specifies second event listener to work when the table is clicked, then it will
+	display the message,'Hey, you clicked me!'.*/ 
+    document.querySelector("table").addEventListener("click", function(){
+        alert('Hey, you clicked me!');
+    });
+}
